6a54fdc2abefe0b22d5c15f33ab83e8fe6943d44
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-10-05 15:37:58 +0530

    Step-7

--- src/component/index.js
+++ src/component/index.js
@@ -1,7 +1,24 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Select from "./Select";
+import Input from "./Input";
+import PostCard from "./PostCard";
+import Login from "./Login";
+
 
 export { 
     Header, 
-    Footer 
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    PostCard,
+    Login,
 };
