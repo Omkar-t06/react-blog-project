df8df50054e5fba0d67514ea143bf75a66d18bcc
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-10-15 21:56:29 +0530

    Step-12

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,17 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import { Container, PostCard } from '../component';
 import databaseService from '../appwrite/config';
+import { BounceLoader } from 'react-spinners';
 
 function AllPosts() {
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         databaseService.getPosts().then(
             (posts) => {
                 if(posts) 
                     setPosts(posts.documents)
+                setLoading(false);
             }
         )
     },[])
+
+    if(loading) {
+        return (
+            <div className='flex items-center justify-center p-10 m-10'>
+                <BounceLoader/>
+            </div>
+        )
+    }
+
   return (
     <div className='w-full py-8'>
         <Container>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import databaseService from '../appwrite/config';
-import { Container, PostCard } from '../component';
+import { Container,  PostCard } from '../component';
 import { useSelector } from 'react-redux';
+import { BounceLoader } from 'react-spinners';
 
 function Home() {
     const [posts, setPosts] = useState([]);
     const userData = useSelector((state) => state.auth.userData);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        databaseService.getPosts().then(
+        databaseService.getPosts([]).then(
             (posts) => {
                 if(posts) {
                     setPosts(posts.documents);
                 }
+                setLoading(false);
             }
         )
     },[])
+
+    if(loading) {
+        return (
+            <div className='flex items-center justify-center p-10 m-10'>
+                <BounceLoader/>
+            </div>
+        )
+    }
 
     if(posts.length === 0) {
         return (
@@ -35,13 +46,31 @@ function Home() {
   return (
     <div className='w-full py-8'>
         <Container>
+            <h1 className='text-2xl m-2'>Hello, <span className='font-bold italic text-gray-800'>{userData?.name}!</span></h1>
             <div className='flex flex-wrap'>
                 {posts.map((post) => (
-                    <div key={post.$id} className='p-2 w-1/4'>
+                    post.status === 'active'  &&(<div key={post.$id} className='p-2 w-1/4'>
                         <PostCard {...post} />
-                    </div>
+                    </div>)
                 ))}
             </div>
+            {posts.some(post => (post.status === 'inactive' && post.userId === userData?.$id)) && 
+            (
+            <>    
+            <div className='flex items-center justify-center'>
+                <hr className='border w-1/2 border-gray-200'/>
+                <span className='bg-white text-md font-bold px-1 rounded-lg whitespace-nowrap'>Your Draft</span>
+                <hr className='border w-1/2 border-gray-200'/>
+            </div>
+            <div className='flex flex-wrap'>
+                {posts.filter(post => post.status == 'inactive').map((post) => (
+                    post.status === 'inactive'  && (<div key={post.$id} className='p-2 w-1/4'>
+                        <PostCard {...post} />
+                    </div>)
+                ))}
+            </div>
+            </>
+            )}
         </Container>
     </div>
   )
